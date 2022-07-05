baa544179d01d5043aac814f39ef8f64c4ef4d70
Magomed <Djambulat@hotmail.com>
2022-07-05 13:24:05 +0200

    projet initial

---
+++ FilRouge/FilRouge/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FilRouge
+//
+//  Created by Student04 on 05/07/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
